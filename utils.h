0c016d4233f4194e19a5791c1f1acc553a25f48e
tapion <tapion@tapion.it>
2012-11-21 20:17:16 +0100

    Commentato utils.h

--- utils.h
+++ utils.h
@@ -1,37 +1,60 @@
-#define BUFSIZE 50
-
+/* la dimensione massima dei pacchetti */
 #define MAXSIZE 6500 /*65536*/
 
+/* la dimensione in byte dell'header dei datagram UDP */
 #define HEADERSIZE 5
 
+/* la dimensione massima del body dei pacchetti */
 #define BODYSIZE (MAXSIZE) - (HEADERSIZE)
 
-#define TIMEOUT 1 /* seconds */
-#define MSTIMEOUT 0
+/* il timeout della select del proxysender */
+#define TIMEOUT 1   /* secondi */
+#define MSTIMEOUT 0 /* microsecondi */
 
+/* struttura del pacchetto UDP */
 typedef struct packet{
+	/* l'ID del paccheto, intero senza segno a 32 bit */
 	uint32_t id;
+	/* il carattere che identifica il tipo di pacchetto:
+	 * 'B' per i normali pacchetti BODY, anche gli ACK
+	 * 'I' per gli ICMP
+	 * 'E' per gli errori interni (aggiunto da noi) */
 	char tipo;
+	/* una stringa per il corpo del pacchetto */
 	char body[BODYSIZE];
 } __attribute__((packed)) packet;
 
+/* la struttura dell'ICMP */
 typedef struct ICMP{
+	/* intero senza segno a 32 bit che rappresenta l'id dell'ICMP */
 	uint32_t id;
+	/* un carattere che rappresenta il tipo 'I' dell'ICMP */
 	char tipo;
+	/* l'ID del pacchetto scartato */
 	uint32_t idpck;
 } __attribute__((packed)) ICMP;
 
+/* la struttura degli elementi delle liste dinamiche utilizzate dai
+ * proxy. Questa struttura è usata anche dalle sentinelle, che però
+ * utilizzano solo il campo next */
 typedef struct lista{
+	/* il puntatore all'elemento successivo */
     struct lista* next;
+    /* il pacchetto */
     packet p;
+    /* il timestamp abbinato al pacchetto */
     struct timeval sentime;
+    /* la dimensione del pacchetto */
     int size;
 } __attribute__((packed)) lista;
+
+/* prototipi delle funzioni di utility definite in "utils.c" */
 
 int timeval_subtract (struct timeval *result,
 				      struct timeval *x,
 				      struct timeval *y);
-
+/* variabile globale che contiene il numero di pacchetti contenuti
+ * in una lista */
 int nlist;
 
 void stampalista(lista* sentinella);
