b8c32a8492194e54a5a778559943254141ba035c
tapion <tapion@tapion.it>
2012-11-22 11:19:18 +0100

    Piccole modifiche

--- utils.h
+++ utils.h
@@ -63,27 +63,16 @@ void aggiungi_in_ordine( lista* sentinella, packet p, int size );
 lista pop(lista* sentinella);
 lista rimuovi(lista* sentinella, uint32_t id);
 
-
 int get_socket(int type);
-
 void sock_opt_reuseaddr(int socketfd);
-
 void name_socket(struct sockaddr_in *opt, uint32_t ip_address, uint16_t port);
-
 void sock_bind(int socketfd, struct sockaddr_in* Local);
-
 void sock_connect(int sock, struct sockaddr_in *opt);
-
 void sock_listen(int sock);
-
 int sock_accept(int socketfd, struct sockaddr_in *opt);
-
 int TCP_connection_send(const char *remote_ip, int remote_port);
-
 int TCP_connection_recv(int local_port);
-
 int UDP_sock(int local_port);
 
 ssize_t readn (int fd, char *buf, size_t n, struct sockaddr_in *from);
-
 void writen (int fd, char *buf, size_t n, struct sockaddr_in *to);
